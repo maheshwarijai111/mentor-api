e6cbf8edcdf48a0335d1b9316cc7dc1be9533cf2
Jai Prakash <maheshwarijai111@gmail.com>
2025-01-25 15:19:47 +0000

    add new column

--- models/User.go
+++ models/User.go
@@ -14,6 +14,7 @@ type User struct {
 	Email        string    `gorm:"uniqueIndex;type:varchar(100);column:email" json:"email"`
 	PasswordHash string    `gorm:"type:varchar(255);column:password_hash" json:"password_hash"`
 	Role         string    `gorm:"type:varchar(50);column:role" json:"role"`
+	Status       int       `gorm:"column:status" json:"status"`
 	Location     string    `gorm:"type:varchar(100);column:location" json:"location"`
 	CreatedAt    time.Time `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt    time.Time `gorm:"column:updated_at" json:"updated_at"`
@@ -25,6 +26,7 @@ type UserList struct {
 	LastName  string `gorm:"type:varchar(100);column:last_name" json:"last_name"`
 	Email     string `gorm:"uniqueIndex;type:varchar(100);column:email" json:"email"`
 	Role      string `gorm:"type:varchar(50);column:role" json:"role"`
+	Status    int    `gorm:"column:status" json:"status"`
 	Location  string `gorm:"type:varchar(100);column:location" json:"location"`
 }
 
